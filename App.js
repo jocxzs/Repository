e689ae57e4ec3190731b42dc5bb28b3cbc0d16f0
jocxzs <joqfra07@gmail.com>
2025-11-23 17:14:43 -0300

    Home and CharList Screens & Updated Dependancies

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import HomeScreen from './src/screens/HomeScreen.js';
+import CharacterList from './src/screens/CharacterList.js';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="CharacterList"
+          component={CharacterList}
+          options={{ 
+            title: 'Character List',
+            headerStyle: { backgroundColor: '#252525ff' },
+            headerTintColor: '#e6e6e6ff',
+            headerTitleStyle: { fontWeight: 'bold' },
+           }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ 
+            title: 'Home',
+            headerStyle: { backgroundColor: '#252525ff' },
+            headerTintColor: '#e6e6e6ff',
+            headerTitleStyle: { fontWeight: 'bold' },
+           }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
